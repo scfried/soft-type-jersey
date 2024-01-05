f783a0baf9cdfa199a16c4a14085dd7856548ba3
Emma Marichal <bonjour@emmamarichal.fr>
2024-01-05 12:04:14 +0100

    Finished config.yaml set up

--- sources/build.sh
+++ sources/build.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 set -e
 
-echo "Build Jacquard 12"
+echo "Build Jersey 10"
 
-gftools builder config-jacquard12.yaml
+gftools builder config-jersey10.yaml
 
-echo "Build Jacquard 12 Charted"
+echo "Build Jersey 10 Charted"
 
-gftools builder config-jacquard12charted.yaml
+gftools builder config-jersey10charted.yaml
 
-echo "Build Jacquard 24"
+echo "Build Jersey 15"
 
-gftools builder config-jacquard24.yaml
+gftools builder config-jersey15.yaml
 
-echo "Build Jacquard 24 Charted"
+echo "Build Jersey 15 Charted"
 
-gftools builder config-jacquard24charted.yaml
+gftools builder config-jersey15charted.yaml
+
+echo "Build Jersey 20"
+
+gftools builder config-jersey20.yaml
+
+echo "Build Jersey 20 Charted"
+
+gftools builder config-jersey20charted.yaml
+
+echo "Build Jersey 25"
+
+gftools builder config-jersey25.yaml
+
+echo "Build Jersey 25 Charted"
+
+gftools builder config-jersey25charted.yaml
 
 echo "Complete"
